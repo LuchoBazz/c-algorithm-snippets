d50cbb63451e578ec384be741e22b307c5de1150
Luis Miguel Báez <es.luismiguelbaez@gmail.com>
2024-04-22 15:02:55 -0500

    feat(template): Update Minimalist Template

---
+++ template/minimalist_template.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+#define forn(i, n) for (int i = 0; i < (n); ++i)
+#define ll long long
+
+#define N (200000 + 5)
+
+ll A[N];
+
+void solve_one() {
+  ;
+}
+
+int main(void) {
+  int T = 1;
+  scanf("%d", &T);
+  while (T-- > 0) {
+    solve_one();
+  }
+  return 0;
+}
