5a59220bc5c7a3603d1f1d15da4a299502e8e2cb
Luis Miguel Báez <es.luismiguelbaez@gmail.com>
2024-04-22 10:08:29 -0500

    feat(template): Update std template

--- template/std_template.c
+++ template/std_template.c
@@ -2,17 +2,24 @@
 #include <string.h>
 #include <sys/time.h>
 
+#ifdef ONLINE_JUDGE // Reference: https://codeforces.com/blog/entry/96344
+#pragma GCC optimize("O3,unroll-loops")
+#pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt")
+#endif
+
 #define forn(i, n) for (int i = 0; i < (n); ++i)
 #define rof(i, n) for (int i = (n)-1; i >= 0; i--)
+#define INF 1000000000
+#define INF64 0x3f3f3f3f3f3f3f3fLL
 #define ll long long
 #define endl "\n"
 
 ll min(ll, ll);
 ll max(ll, ll);
-void sort(ll *, int, int);
+void sort(int *, int, int);
 void init_random();
 
-#define N (200000 + 5)
+#define N (100000 + 5)
 
 int n, m;
 // char s[N];
@@ -37,7 +44,8 @@ int main() {
 // int len = strlen(s)
 // strcpy, strcat, strcmp, strncpy
 // input:
-// %s (char*) %d (int) %lld (ll) %c (char) %f (float) %lf (double) %u (unsigned) %x (Hexadecimal) %o (Octal)
+// %s (char*), %d (int), %lld (ll), %c (char), %f (float), %lf (double),
+// %u (unsigned), %x (Hexadecimal), %o (Octal)
 
 ll min(ll a, ll b) { return a < b ? a : b; }
 ll max(ll a, ll b) { return a > b ? a : b; }
@@ -51,10 +59,10 @@ void init_random() {
   Z = tv.tv_sec ^ tv.tv_usec | 1;
 }
 
-void sort(ll *array, int left, int right) { // [left, right)
+void sort(int *array, int left, int right) { // [left, right)
   while (left < right) {
-    int pivot_index = left, low = left, high = right;
-    ll pivot_value = array[left + get_random() % (right - left)], temp;
+    int pivot_index = left, low = left, high = right,
+        pivot_value = array[left + get_random() % (right - left)], temp;
 
     while (low < high) {
       if (array[low] == pivot_value)
