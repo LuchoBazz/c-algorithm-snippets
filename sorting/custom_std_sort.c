4a6cdf3896dd740775a49b17654652486e611b0d
Luis Miguel Báez <es.luismiguelbaez@gmail.com>
2024-04-23 15:05:10 -0500

    feat(sorting): Update custom sorting

--- sorting/custom_std_sort.c
+++ sorting/custom_std_sort.c
@@ -8,12 +8,14 @@ sort_type compare(const void *a, const void *b) {
   return first - second;
 }
 
-void sort(sort_type *array, int n) {
+void sort(sort_type *array, int n,
+          sort_type (*compare_array)(sort_type, sort_type)) {
   srand(time(NULL));
   sort_type tmp;
   for (int i = 0, j; i < n; i++) {
     j = rand() % (i + 1);
     tmp = array[i], array[i] = array[j], array[j] = tmp;
   }
-  qsort(array, n, sizeof *array, compare);
+  qsort(array, n, sizeof *array, compare_array);
 }
+// sort(A, n, compare);
