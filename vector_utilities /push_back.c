b7678f2dac004d4dc2c5cfebaa1d734c855b611b
Luis Miguel Báez <es.luismiguelbaez@gmail.com>
2024-04-26 11:16:32 -0500

    feat(vector): improve push_back so that it does not need to be mallocated first

--- vector_utilities /push_back.c
+++ vector_utilities /push_back.c
@@ -1,33 +1,31 @@
 
 #define pb_type int
 void push_back(pb_type **vec, int *len, pb_type value) {
-  int index = (*len);
+  int index = (*len)++;
   int is_power_of_two = (index & (index - 1)) == 0;
-  if (index >= 2 && is_power_of_two) {
+  if (index == 0) {
+    *vec = (pb_type *)malloc(2 * sizeof(pb_type));
+  } else if (index >= 2 && is_power_of_two) {
     *vec = (pb_type *)realloc(*vec, index * 2 * sizeof(pb_type));
   }
   (*vec)[index] = value;
-  (*len)++;
 }
 
 
 // -----------------------------------------------------------------
 // Vector
 // int v_len = 0;
-// int *vec = (int *) malloc(2 * sizeof(int));
+// int *vec = NULL;
 // push_back(&vec, &v_len, 1);
 // push_back(&vec, &v_len, 2);
 // for (int i = 0; i < v_len; ++i) printf("%d ", vec[i]);
+// free(vec);
 // Ouput: 1 2
 // -----------------------------------------------------------------
 // Matrix NxM
 // #define N 2
 // int *grid[N];
 // int grid_cnt[N] = {0};
-//
-// for (int i = 0; i < N; ++i) {
-//   grid[i] = (int *)malloc(2 * sizeof(*grid[i]));
-// }
 // 
 // push_back(&grid[0], &grid_cnt[0], 10);
 // push_back(&grid[0], &grid_cnt[0], 20);
@@ -38,6 +36,7 @@ void push_back(pb_type **vec, int *len, pb_type value) {
 //   for (int j = 0; j < grid_cnt[i]; ++j) printf("%d ", grid[i][j]);
 //   putchar('\n');
 // }
+// for (int i = 0; i < ...; ++i) free(grid[i]);
 // Ouput:
 // 10 30 
 // 20 40 
