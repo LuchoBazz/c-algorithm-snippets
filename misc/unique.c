22bd368365f6a8d26281e8ee66228ab4be1db165
Luis Miguel Báez <es.luismiguelbaez@gmail.com>
2024-04-27 20:43:08 -0500

    feat(unique): Update unique function

--- misc/unique.c
+++ misc/unique.c
@@ -1,14 +1,21 @@
-// #define ll long long
 
-void unique(ll *destination, int *dest_size, ll *source, int src_size) {
-  // sort(source, 0, src_size); // requires that source is sorted
-  destination[0] = source[0];
+#define unique_type int
+unique_type *make_unique(unique_type *src, int src_sz, int *dest_size) {
+  unique_type *src_cloned = (unique_type *)malloc(src_sz * sizeof(unique_type));
+  unique_type *dest = (unique_type *)malloc(src_sz * sizeof(unique_type));
+  for (int i = 0; i < src_sz; ++i) src_cloned[i] = src[i];
+  sort(src_cloned, src_sz, compare);
+  dest[0] = src_cloned[0];
   int dest_index = 0;
-  for(int i = 0; i < src_size; ++i) {
-    if (source[i] != destination[dest_index]) {
+  for (int i = 0; i < src_sz; ++i) {
+    if (src_cloned[i] != dest[dest_index]) {
       dest_index++;
     }
-    destination[dest_index] = source[i];
+    dest[dest_index] = src_cloned[i];
   }
   *dest_size = dest_index + 1;
+  return dest;
 }
+
+// int unique_sz = 0;
+// int *unique = make_unique(A, n, &unique_sz);
